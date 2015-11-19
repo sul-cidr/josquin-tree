e288e1b83b7231444dcd2327a0e6bc01950fa395
David McClure <davidwilliammcclure@gmail.com>
2015-11-19 14:52:37 -0800

    Start to block in test code for the cluster layout, with Bostock's flare data.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,6 +1,22 @@
 
 
-import _ from 'lodash';
+import d3 from 'd3';
+import data from './flare.json';
 
 
-console.log(_);
+let h = 1000;
+let w = 1000;
+
+let cluster = d3.layout.cluster()
+  .size([h, w]);
+
+let diagonal = d3.svg.diagonal()
+  .projection(function(d) {
+    return [d.y, d.x];
+  });
+
+let svg = d3.select('#root')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h)
+  .append('g');
