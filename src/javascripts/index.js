f4df3e8b0ebec9d29551e0e90f5ab70abf3cf8cc
David McClure <davidwilliammcclure@gmail.com>
2015-11-30 14:20:33 -0800

    Stubbing in the basic d3 dendrogram instantiation.

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,8 +4,8 @@ import d3 from 'd3';
 import data from './flare.json';
 
 
-let h = 1000;
-let w = 1000;
+let w = 700;
+let h = 1700;
 
 let cluster = d3.layout.cluster()
   .size([h, w]);
@@ -20,3 +20,34 @@ let svg = d3.select('#root')
   .attr('width', w)
   .attr('height', h)
   .append('g');
+
+let nodes = cluster.nodes(data);
+let links = cluster.links(nodes);
+
+let link = svg.selectAll('.link')
+  .data(links)
+  .enter()
+  .append('path')
+  .attr('class', 'link')
+  .attr('d', diagonal);
+
+let node = svg.selectAll('.node')
+  .data(nodes)
+  .enter()
+  .append('g')
+  .attr('class', 'node')
+  .attr('transform', function(d) {
+    return `translate(${d.y},${d.x})`;
+  });
+
+node.append('circle')
+  .attr('r', 4);
+
+node.append('text')
+  .attr('dx', function(d) {
+    return d.children ? -8 : 8;
+  })
+  .attr('dy', 3)
+  .text(function(d) {
+    return d.name;
+  });
