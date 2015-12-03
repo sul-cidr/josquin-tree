dd4e7c94097c1b3bbcbd4d94a659db7e1b84ca33
David McClure <davidwilliammcclure@gmail.com>
2015-12-02 17:17:02 -0800

    Scrub out 'X' piece delimiters in the notes input sequence.

--- tasks.py
+++ tasks.py
@@ -11,11 +11,13 @@ def make_notes():
 
     notes = ''
 
+    # Mush together files.
     for entry in os.scandir('data/notes'):
         with open(entry.path, 'r') as fh:
             notes += fh.read()
 
     notes = notes.replace('\n', ' ').split(' ')
+    notes = [n for n in notes if n is not 'X']
 
     with open('src/javascripts/data/notes.json', 'w') as fh:
         json.dump(notes, fh)
